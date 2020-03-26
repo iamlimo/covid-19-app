486d6170c913204ede8d308531b8b3507104156d
Isaac orija <isaac.orija@gmail.com>
2020-03-26 16:25:00 +0100

    first update

---
+++ script.js
@@ -0,0 +1,12 @@
+// var i = 0;
+// var txt =`<h1 class="headerText text-center">Let's Help Keep Naija Safe</h1>
+// <p class="headerPara text-center">Better health is enjoyed by those who donate their time, expertise or money to others who are in need. </p>`;
+// var speed = 50;
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("headers").textContent += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
