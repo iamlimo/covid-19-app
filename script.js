3ef65e40a8d6380e1ee5c5b94e723efc1744f8e2
Isaac orija <isaac.orija@gmail.com>
2020-03-28 14:19:01 +0100

    new updates

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-// var i = 0;
-// var txt =`<h1 class="headerText text-center">Let's Help Keep Naija Safe</h1>
-// <p class="headerPara text-center">Better health is enjoyed by those who donate their time, expertise or money to others who are in need. </p>`;
-// var speed = 50;
+google.charts.load('current', { 'packages': ['map'],
+'mapsApiKey': 'AIzaSyAAhy_9WWHiIYdANue9UquWcrhSQ8wPJiQ'
+});
+google.charts.setOnLoadCallback(drawMap);
 
-// function typeWriter() {
-//   if (i < txt.length) {
-//     document.getElementById("headers").textContent += txt.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, speed);
-//   }
-// }
+function drawMap() {
+  var data = google.visualization.arrayToDataTable([
+  ['Lat', 'Long', 'Name'],
+[6.524379,3.379206, 'Lagos'],
+[7.131980, 3.642200, 'Ogun State'],
+[9.076479, 7.398574, 'Abuja'],
+[6.444550, 7.490180, 'Enugu'],
+[10.314159, 9.846282, 'Bauchi'],
+[7.292550, 7.697180, 'Edo'],
+[7.706490, 5.365730, 'Ekiti'],
+[7.544510, 4.556030, 'Osun'],
+[7.842958, 3.936844, 'Oyo'],
+[5.022600, 6.973740, 'Rivers']
+  ]);
+
+var options = {
+  showTooltip: true,
+  showInfoWindow: true
+
+};
+
+var map = new google.visualization.Map(document.getElementById('chart_div'));
+
+map.draw(data, options);
+};
